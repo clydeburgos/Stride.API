72f4697506e991cdfdcd2c4cd333c2fb7c6a0d91
Vincent Clyde Burgos <clydeburgos@gmail.com>
2023-05-09 11:35:22 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+IConfiguration configuration = builder.Configuration;
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/Configs/nlog.config"));
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureStrideServices(configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
